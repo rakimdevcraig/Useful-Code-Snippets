0bc05450236fdcd0bcf57dacbdea5504b2dd1e02
rakimdevcraig <rakimdevcraig@gmail.com>
2020-01-07 19:16:53 -0500

    added folder for node 12 import/export

---
+++ import:exportmodules/one.js
@@ -0,0 +1,10 @@
+//to use import/export have to put   "type": "module" in package.json
+//old way
+// let lastName = require("./two.js")
+
+
+//es6 way
+import testName from "./two.js";
+
+console.log(testName)
+// lastName()
---
+++ import:exportmodules/two.js
@@ -0,0 +1,16 @@
+//to use import/export have to put   "type": "module" in package.json
+
+
+function lastName(){
+    console.log('craig')
+}
+
+let testName = 'john jenry'
+
+
+
+//es6 way
+export default testName;
+
+//old way
+// module.exports = lastName;
